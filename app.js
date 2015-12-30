e87f3e5a96c87b3b5c90c433551b2e90e8f3a39a
Cairo Stewart <stewartcairo@gmail.com>
2015-12-30 15:12:15 -0800

    add email

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express'),
 app = express(),
 router = require('./controllers/routes'),
-morgan = require('morgan');
+morgan = require('morgan'),
+favicon = require('serve-favicon'),
+bodyParser = require('body-parser');
+
 
 app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static('public'))
+
 
 app.use('/', router);
 app.listen(process.env.PORT || 8000, function() {
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-router = express.Router();
+router = express.Router(),
+nodemailer = require('nodemailer');
+var smtTrans, mailOpts;
 
 router.get('/',function(req,res) { 
   res.render('pages/index')
@@ -16,5 +18,33 @@ router.get('/prices',function(req,res) {
 router.get('/contact',function(req,res) { 
   res.render('pages/contact')
  });
+
+router.get('/email',function(req,res) { 
+  res.render('pages/email');
+ });
+
+router.post('/email',function(req,res) { 
+
+	var transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+	    user: 'infoilovedogwalks@gmail.com',
+	    pass: 'TotoTigger1982'
+	  }
+	}, {
+	// default values for sendMail method 
+	from: req.body.email,
+	headers: {
+	    'My-Awesome-Header': '123'
+	}
+	});
+
+	transporter.sendMail({
+	    to: 'infoilovedogwalks@gmail.com',
+	    subject: 'Work Email!!!',
+	    text: req.body.message
+	});
+  res.redirect('/contact');
+ })
 
 module.exports = router;
